7b29e1f45f082bf62a0a95bccb791f09cffa5a2c
Pravin Vaz <pravinvaz@gmail.com>
2025-12-01 13:36:38 +1100

    test

--- Mixed_Examples/AreaOfTriangle.py
+++ Mixed_Examples/AreaOfTriangle.py
@@ -1,4 +1,12 @@
 '''This program will accept some values of the sides of a triangle to spit  out 
 the area. 
 '''
+base = float(input("Enter the base of the trianglein metres: "))
+height = float(input("Enter the height of the triangle in metres: "))
+area = 0.5*base*height
+print("The area of the triangle is: ", area, "sq.m")
 
+# challenges
+# Create an area calculator for a square using the same concept
+# Combine the square and triangle calculators together, so the user gets the option to choose what they want to calculate
+# Create functions
